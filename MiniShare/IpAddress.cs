87c257b7a47e4b5148c1754fe72dae002d0b3672
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-09-24 16:13:59 +0300

    Add files via upload

---
+++ MiniShare/IpAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniShare
+{
+    public static class IpAddress
+    {
+        public static string LocalIpAddress { get; private set; }
+        static IpAddress()
+        {
+            LocalIpAddress = Utilities.LocalIPAddress();
+        }
+    }
+}
