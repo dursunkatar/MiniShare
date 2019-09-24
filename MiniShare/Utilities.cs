420ef14fcc536156b955bb48da17b38f5edfb7c2
Dursun KATAR <45147011+dursunkatar@users.noreply.github.com>
2019-09-24 15:21:27 +0300

    Add files via upload

---
+++ MiniShare/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MiniShare
+{
+    public static class Utilities
+    {
+        public static string LocalIPAddress()
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                return null;
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            return host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+        }
+        public static string SharedPathFix(string sharedPath)
+        {
+            return sharedPath
+                .ToLower(new CultureInfo("tr-TR", false))
+                .Replace('ı', 'i')
+                .Replace('ü', 'u')
+                .Replace('ş', 's')
+                .Replace('ö', 'o')
+                .Replace(' ', '-');
+        }
+    }
+
+}
